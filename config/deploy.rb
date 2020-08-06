0e9fd1aacfaa68134f88fdb543c703722c75a241
Santhosh Gade <santhosh.gade@omma.com>
2020-08-06 16:46:37 +0530

    updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,6 +34,6 @@ set :pty, true
 
 # Default value for keep_releases is 5
 set :keep_releases, 2
-after 'deploy', 'mytest'
+after 'deploy', 'db_migration'
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
